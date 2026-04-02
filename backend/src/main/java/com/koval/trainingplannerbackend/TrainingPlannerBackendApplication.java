212195739baeabd11f9aadf51cd7b1815ae193cb
GAR <gauthier.robert@telemis.com>
2026-04-02 17:30:50 +0200

    fix: Exclude Rabbit if not enable

--- backend/src/main/java/com/koval/trainingplannerbackend/TrainingPlannerBackendApplication.java
+++ backend/src/main/java/com/koval/trainingplannerbackend/TrainingPlannerBackendApplication.java
@@ -1,12 +1,13 @@
 package com.koval.trainingplannerbackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.amqp.autoconfigure.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RabbitAutoConfiguration.class)
 @EnableAsync
 @EnableScheduling
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
